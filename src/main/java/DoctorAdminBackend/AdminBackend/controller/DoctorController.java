714bda20261e9b221626c224bdbc473b02d5915f
pradip patel <patel9826ravi@gmail.com>
2025-01-10 05:42:11 +0530

    changes

--- src/main/java/DoctorAdminBackend/AdminBackend/controller/DoctorController.java
+++ src/main/java/DoctorAdminBackend/AdminBackend/controller/DoctorController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import DoctorAdminBackend.AdminBackend.Model.Doctor;
 import DoctorAdminBackend.AdminBackend.ServiceIMPL.DoctorService;
 
@@ -22,7 +22,7 @@ public class DoctorController {
     @Autowired
     private DoctorService doctorService;
 
-
+    @CrossOrigin(origins = "http://localhost:8084")
     @GetMapping("/with-patients")
     public ResponseEntity<Map<String, Object>> getDoctorsWithPatients() {
         List<Map<String, Object>> doctorsWithPatients = doctorService.getDoctorsWithPatients();
